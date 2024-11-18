2244ae21672c6cd48ed984059ecd5566f21354a9
izzahbalqis112 <n.izzahbalqis@gmail.com>
2024-11-18 14:01:56 +0800

    Update localNotifications.dart

--- lib/main/homepage/more/notification/localNotifications.dart
+++ lib/main/homepage/more/notification/localNotifications.dart
@@ -4,9 +4,7 @@ class LocalNotifications{
   static final FlutterLocalNotificationsPlugin
   _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
-  //initializw the local notifications
   static Future init() async {
-    // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
     const AndroidInitializationSettings initializationSettingsAndroid =
     AndroidInitializationSettings('@mipmap/ic_launcher');
     final DarwinInitializationSettings initializationSettingsDarwin =
@@ -24,7 +22,6 @@ class LocalNotifications{
     );
   }
 
-  //show simple notifications
   static Future showSimpleNotification({
     required String title,
     required String body,
